ca9396be3048836c33f5e5b2257ec58ca154598e
lachlanbell <me@lachlanbell.com.au>
2016-08-02 19:24:45 +0800

    Complete serial_client.py
    Update to connect to server, rather host.

--- raspberryPi/serial_client.py
+++ raspberryPi/serial_client.py
@@ -13,16 +13,23 @@ from socket import *
 import time
 import serial
 
-# intialise TCP connection
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.connect(('192.168.1.100', 8003))
-server_socket.listen(0)
-buffer = 20
+# intialise/set variables
+buffer = 20 # TCP buffer
+ri = "" # Received instruction
+pd = "" # Previous sent data
+
+# intialise TCP connection, connect to sever on Mac.
+cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cs.connect(('192.168.1.100', 8003))
+cs.listen(0)
 
 # intialise serial connection to Arduino
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
-con , addr = s.accept()
+con , addr = cs.accept()
 print("Connection address:", addr)
 while 1:
-  data = con.recv(buffer)
+  ri = con.recv(buffer)
+  if ri != pd:
+            ser.write(ri)
+            pd = ri
